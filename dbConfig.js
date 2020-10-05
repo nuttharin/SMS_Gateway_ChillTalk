4aa190e116830908574e8c359f5aa4aa75bbe0a5
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2020-10-05 15:51:36 +0700

    pool

--- dbConfig.js
+++ dbConfig.js
@@ -12,31 +12,32 @@ const pool = new Pool({
     connectionString:  connectionStringPostgres   
 });
 //console.log(pool);
-pool.connect((err, client, release) => {
-    if (err) {
-      return console.error('Error acquiring client', err.stack)
-    }
-    client.query('SELECT NOW()', (err, result) => {
-      release()
-      if (err) {
-        return console.error('Error executing query', err.stack)
-      }
-      console.log("connect")
-      console.log(result.rows)
-    })
-  })
-const client = new Client({
-    host: process.env.DB_POSTGRES_HOST,
-    port: process.env.DB_POSTGRES_PORT,
-    user: process.env.DB_POSTGRES_USER,
-    password: process.env.DB_POSTGRES_PASSWORD,
-    database : process.env.DB_POSTGRES_DATABASE
-});
-console.log(client);
-client
-  .connect()
-  .then(() => console.log('connected'))
-  .catch(err => console.error('connection error', err.stack))
+// pool.connect((err, client, release) => {
+//     if (err) {
+//       return console.error('Error acquiring client', err.stack)
+//     }
+//     client.query('SELECT NOW()', (err, result) => {
+//       release()
+//       if (err) {
+//         return console.error('Error executing query', err.stack)
+//       }
+//       console.log("connect")
+//       console.log(result.rows)
+//     })
+//   })
+// const client = new Client({
+//     host: process.env.DB_POSTGRES_HOST,
+//     port: process.env.DB_POSTGRES_PORT,
+//     user: process.env.DB_POSTGRES_USER,
+//     password: process.env.DB_POSTGRES_PASSWORD,
+//     database : process.env.DB_POSTGRES_DATABASE
+// });
+// console.log(client);
+// client
+//   .connect()
+//   .then(() => console.log('connected'))
+//   .catch(err => console.error('connection error', err.stack))
+
 module.exports = {
     pool,
     client,
