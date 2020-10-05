a30ab5aad0d52f6aa2383a3b5362f8f8453648df
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2020-10-05 15:14:57 +0700

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,5 +1,5 @@
 const MongoClient  = require('mongodb').MongoClient ;
-const { Pool } = require("pg");
+const { Pool ,Client } = require("pg");
 
 const URL_MONGODB_IOT = process.env.URL_MONGODB +process.env.DATABASE_DATA_IOT ;
 
@@ -12,6 +12,16 @@ const pool = new Pool({
     connectionString:  connectionStringPostgres   
 });
 console.log(pool);
+
+const client = new Client({
+    host: process.env.DB_POSTGRES_HOST,
+    port: process.env.DB_POSTGRES_PORT,
+    user: process.env.DB_POSTGRES_USER,
+    password: process.env.DB_POSTGRES_PASSWORD,
+    database : process.env.DB_POSTGRES_DATABASE
+});
+console.log(client);
+
 module.exports = {
     pool,
     MongoClient,
