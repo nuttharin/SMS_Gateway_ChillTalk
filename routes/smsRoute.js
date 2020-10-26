4d3a6a435714041da9ef28c82db377bc01c6236b
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2020-10-26 11:59:17 +0700

    Update smsRoute.js

--- routes/smsRoute.js
+++ routes/smsRoute.js
@@ -7,6 +7,7 @@ const { ensureToken } = require('../controllerrs/authController');
 
 const smsController = require('../controllerrs/smsController');
 const mtController = require('../controllerrs/mtController');
+const appGasController = require('../controllerrs/appGasController')
 
 // get
 router.get('/get/test',smsController.test);
@@ -17,11 +18,16 @@ router.get('/get/test',smsController.test);
 router.post('/post/sendSMSInvitation',ensureToken,smsController.sendSMSInvitation);
 
 // router.post('/post/sendSMSMoneyTranfer',ensureToken,mtController.sendSMSMoneyTranfer);
+//mt
 router.post('/mt/post/sendMessage',mtController.sendSMSMt);
 router.post('/mt/post/sendOtp',mtController.sendSmsOtp);
 router.post('/mt/post/checkOtp',mtController.checkOtp);
 
- 
+
+
+//App Gas
+router.post('/mt/post/sendOtp',appGasController.sendSmsOtp);
+router.post('/mt/post/checkOtp',appGasController.checkOtp);
 
 
 module.exports = router;
